51c23d787ac48a09b8558910e2223c42430ed168
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-07 13:18:54 +0900

    [Bronze II] Title: 16진수, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine(),16);
+        System.out.println(a);
+    }
+}
