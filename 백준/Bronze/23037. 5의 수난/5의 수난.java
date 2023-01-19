8d5415958377546e243cc26e1951a10fa03720fc
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-19 17:41:12 +0900

    [Bronze II] Title: 5의 수난, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/23037. 5의 수난/5의 수난.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		long result = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] array = br.readLine().split("");
+		for(int i = 0; i<array.length; i++) {
+			result+= Math.pow((Long.parseLong(array[i])),5);
+		}
+		System.out.println(result);
+	}
+}
