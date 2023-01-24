70d02f1ba2a89df546535f67fb4fd9d1a91b0ac4
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-24 21:41:41 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 14996 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,15 +1,16 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		int a = sc.nextInt();
-		for (int i = 1; i <= a; i++) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 1; i <= num; i++) {
 			for (int j = 1; j <= i; j++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		sc.close();
+		br.close();
 	}
 }
