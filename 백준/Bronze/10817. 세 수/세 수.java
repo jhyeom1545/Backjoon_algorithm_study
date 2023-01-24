32748e2d2b158a379fb88bb0861eb6020dc19d0e
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-24 15:25:17 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] arr = br.readLine().split(" ");
+		int num1 = Integer.parseInt(arr[0]);
+		int num2 = Integer.parseInt(arr[1]);
+		int num3 = Integer.parseInt(arr[2]);
+		int[] intArr = { num1, num2, num3 };
+		Arrays.sort(intArr);
+		System.out.println(intArr[1]);
+		
+	}
+}
