c9463146e6bd5322c21dabb17ac896980b8ba431
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-25 15:14:29 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		while(true) {
+			String[] arr = br.readLine().split(" ");
+			int a = Integer.parseInt(arr[0]);
+			int b = Integer.parseInt(arr[1]);
+			if(a ==0 && b==0) {
+				break;
+			}
+			System.out.println(a+b);
+		}
+	}
+}
