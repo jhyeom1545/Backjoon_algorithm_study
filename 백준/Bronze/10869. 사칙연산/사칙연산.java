f5680f11f0cef1634425127a257fe4d15dfae114
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-01-25 09:33:14 +0900

    [Bronze V] Title: 사칙연산, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] numArr = br.readLine().split(" ");
+		int a = Integer.parseInt(numArr[0]);
+		int b = Integer.parseInt(numArr[1]);
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+}
+	
