43d0e0c324daa6f8e6424d60c582b26c1147e360
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-05-11 10:20:10 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,12 +1,18 @@
+from collections import deque
+
 def solution(k, score):
-    answer = []  # 정답 리스트
-    a=[]  # 1등 후보 리스트
-    for i in score:  # score 리스트의 요소를 하나씩 탐색
-        if len(a)<k:  # a 리스트에 저장된 수가 k개 이하이면
-            a.append(i)  # a 리스트에 해당 요소 추가
-        else:  # a 리스트에 저장된 수가 k개 이상이면
-            if min(a)<i:  # a 리스트에 저장된 수 중 가장 작은 수보다 i가 크다면
-                a.remove(min(a))  # a 리스트에서 가장 작은 수 삭제
-                a.append(i)  # a 리스트에 해당 요소 추가
-        answer.append(min(a))  # 현재까지 a 리스트에서 가장 작은 수 추가
-    return answer  # 정답 리스트 반환
+    answer = []
+    a = deque()
+    a.append(3) # 처음에 더함
+    a.append(2)
+    print(a)
+    print(a.pop())
+    # for i in range(len(score)):
+    #     if len(a) == k:
+    #         answer.
+    #         answer = answer.pop(-1)
+    #     else:
+    #         answer.insert(0,score[i])
+        
+        
+    return answer
