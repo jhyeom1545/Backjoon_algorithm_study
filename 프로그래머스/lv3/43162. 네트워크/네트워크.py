87585b97c78cc8947c196fba899433948f1e4a9c
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-05-24 22:04:44 +0900

    [level 3] Title: 네트워크, Time: 0.41 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -1,20 +1,20 @@
 def solution(n, computers):
     answer = 0
-    visited = [False for i in range(n)]
-    for com in range(n):
-        if visited[com] == False:
-            BFS(n, computers, com, visited)
-            answer += 1
-    return answer
+    visited = [0] * n  # 방문 여부를 저장하는 리스트 초기화
 
-def BFS(n, computers, com, visited):
-    visited[com] = True
-    queue = []
-    queue.append(com)
-    while len(queue) != 0:
-        com = queue.pop(0)
-        visited[com] = True
-        for connect in range(n):
-            if connect != com and computers[com][connect] == 1:
-                if visited[connect] == False:
-                    queue.append(connect)
+    def dfs(computers, visited, start):
+        stack = [start] # 0으로 시작
+        while stack: # stack에 뭔가 있으면 while
+            j = stack.pop() # 하나 뽑음
+            if visited[j] == 0: # 방문 하지 않았으면
+                visited[j] = 1 # 방문으로 변경
+            for i in range(len(computers)): # computers의 len 만큼 반복문
+                if computers[j][i] == 1 and visited[i] == 0: # computers[0][0]== 1이면 and visited[i] 방문 하지 않았으면 append 마지막에 하나 더하기
+                    stack.append(i)
+
+    for i in range(n):
+        if visited[i] == 0: # false
+            dfs(computers, visited, i)
+            answer += 1
+
+    return answer
