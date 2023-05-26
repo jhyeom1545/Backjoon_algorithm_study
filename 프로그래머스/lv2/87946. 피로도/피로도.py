2fbd0295c4473853111920f84e3dd8759c7b4dda
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-05-26 09:29:54 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -0,0 +1,16 @@
+from itertools import permutations
+def solution(k, dungeons):
+    dungeons_count = len(dungeons)
+    dungeons_list = [i for i in range(dungeons_count)]
+    for i in range(dungeons_count,0,-1):
+        for cases in permutations(dungeons_list,i):
+            now = k
+            check = True
+            for case in cases:
+                if now < dungeons[case][0]:
+                    check = False
+                    break
+                else:
+                    now -= dungeons[case][1]
+            if check:
+                return i
