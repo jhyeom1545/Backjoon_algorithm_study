160078d2d141b32423609a92bc9d1e1e2b327c3b
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-09-24 20:14:23 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,6 +1,2 @@
 def solution(my_string):
-    answer = []
-    for i in my_string:
-        answer.append(i.lower())
-    answer = sorted(answer)
-    return "".join(answer)
+    return ''.join(sorted(my_string.lower()))
