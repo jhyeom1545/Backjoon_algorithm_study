78883917762b222f5558b604b18376b81606d868
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-09-29 17:09:10 +0900

    [unrated] Title: 리스트 자르기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181897. 리스트 자르기/리스트 자르기.py
@@ -0,0 +1,15 @@
+def solution(n, slicer, num_list):
+    a = slicer[0]
+    b = slicer[1]
+    c = slicer[2]
+    
+    answer = []
+    if n == 1:
+        return num_list[0:b+1]
+    elif n == 2:
+        return num_list[a:]
+    elif n == 3:
+        return num_list[a:b+1]
+    else:
+        new_numlist = num_list[a:b+1]
+        return new_numlist[::c]
