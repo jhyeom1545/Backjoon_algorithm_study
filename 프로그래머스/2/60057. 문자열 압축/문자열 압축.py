e9a314c3446e579f609102c7e211f2c21a136f59
JUNHO YEOM <105836661+jhyeom1545@users.noreply.github.com>
2023-12-10 14:24:55 +0900

    [level 2] Title: 문자열 압축, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -0,0 +1,18 @@
+def solution(s):
+    answer = len(s)
+    # 1개 단위(step)부터 압축 단위를 눌러가며 확인
+    for step in range(1, len(s) // 2 + 1):
+        compressed = ""
+        prev = s[0:step] # 앞에서부터 step만큼의 문자열 추출
+        count = 1
+        for j in range(step, len(s), step):
+            if prev == s[j:j + step]:
+                count += 1
+            else:
+                compressed += str(count) + prev if count >= 2 else prev
+                prev = s[j:j + step]
+                count = 1
+        compressed += str(count) + prev if count >= 2 else prev
+        # 만들어지는 압축 문자열이 가장 짧은 것이 정답
+        answer = min(answer, len(compressed))
+    return answer
